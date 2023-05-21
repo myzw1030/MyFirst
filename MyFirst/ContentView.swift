12cc99811b8f828d2f5267d123502af33457d2e6
tommy <tmtmtoa@gmail.com>
2023-05-21 23:15:29 +0900

    button

--- MyFirst/ContentView.swift
+++ MyFirst/ContentView.swift
@@ -7,7 +7,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!aaaaa")
+        
+        VStack {
+            Text("Hello, world!")
+                .font(.largeTitle)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
